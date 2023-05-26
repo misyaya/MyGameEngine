8460f3017f7eddfb15a4f1fee75f738d6eac0c83
彩姫 三澤 <220285@jc-21.jp>
2023-05-26 11:34:10 +0900

    シェーダー　頂点インプットレイアウトまで

--- Direct3D.cpp
+++ Direct3D.cpp
@@ -1,3 +1,4 @@
+#include <d3dcompiler.h>
 #include "Direct3D.h"
 
 
@@ -5,13 +6,13 @@
 namespace Direct3D
 {
 	ID3D11Device* pDevice = nullptr;		//デバイス
-
 	ID3D11DeviceContext* pContext = nullptr;		//デバイスコンテキスト
-
 	IDXGISwapChain* pSwapChain = nullptr;		//スワップチェイン
-
 	ID3D11RenderTargetView* pRenderTargetView = nullptr;	//レンダーターゲットビュー
 
+	ID3D11VertexShader* pVertexShader = nullptr;	//頂点シェーダー
+	ID3D11PixelShader* pPixelShader = nullptr;		//ピクセルシェーダー
+	ID3D11InputLayout* pVertexLayout = nullptr;	//頂点インプットレイアウト
 }
 
 
@@ -84,10 +85,32 @@ void Direct3D::Initialize(int winW, int winH, HWND hWnd)
 	pContext->OMSetRenderTargets(1, &pRenderTargetView, nullptr);            // 描画先を設定
 	pContext->RSSetViewports(1, &vp);
 
-
+	//シェーダー準備
+	InitShader();
 }
 
+//シェーダー準備
+void Direct3D::InitShader()
+{
+	// 頂点シェーダの作成（コンパイル）
+	ID3DBlob* pCompileVS = nullptr;
+	D3DCompileFromFile(L"Simple3D.hlsl", nullptr, nullptr, "VS", "vs_5_0", NULL, 0, &pCompileVS, NULL);
+	pDevice->CreateVertexShader(pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), NULL, &pVertexShader);
+	
+	//頂点インプットレイアウト
+	D3D11_INPUT_ELEMENT_DESC layout[] = {
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },	//位置
+	};
+	pDevice->CreateInputLayout(layout, 1, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &pVertexLayout);
+	
+	pCompileVS->Release();
 
+	// ピクセルシェーダの作成（コンパイル）
+	ID3DBlob* pCompilePS = nullptr;
+	D3DCompileFromFile(L"Simple3D.hlsl", nullptr, nullptr, "PS", "ps_5_0", NULL, 0, &pCompilePS, NULL);
+	pDevice->CreatePixelShader(pCompilePS->GetBufferPointer(), pCompilePS->GetBufferSize(), NULL, &pPixelShader);
+	pCompilePS->Release();
+}
 
 //描画開始
 void Direct3D::BeginDraw()
